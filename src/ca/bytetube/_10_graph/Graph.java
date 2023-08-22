0a910e954223482163033701296a3bfc72b63690
Xinhan Yang <ambpoeialxi@gmail.com>
2023-08-22 19:31:51 -0400

    byte tube coding practice java the shortest path

--- src/ca/bytetube/_10_graph/Graph.java
+++ src/ca/bytetube/_10_graph/Graph.java
@@ -1,11 +1,18 @@
 package ca.bytetube._10_graph;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public abstract class Graph<V, E> {
+    WeightManager<E> weightManager;
     public Graph(){
     }
 
+    public Graph(WeightManager<E> weightManager) {
+        this.weightManager = weightManager;
+    }
 
     public abstract int verticesSize();
 
@@ -30,9 +37,105 @@ public abstract class Graph<V, E> {
     public abstract void dfs(V begin);
 
     public abstract List<V> toPoLogicalSort(V begin);
+
+    public abstract Map<V, E> shortestPathWithoutPathInfos(V begin);
+
+    public abstract Map<V, PathInfo<V, E>> shortestPath(V begin);
+
+    public static class PathInfo<V, E>{
+        protected E weight;
+        protected List<EdgeInfo<V, E>> edgeInfos = new LinkedList<>();
+
+        public PathInfo() {
+
+        }
+
+        public PathInfo(E weight) {
+            this.weight = weight;
+        }
+
+        public E getWeight() {
+            return weight;
+        }
+
+        public void setWeight(E weight) {
+            this.weight = weight;
+        }
+
+        public List<EdgeInfo<V, E>> getEdgeInfos() {
+            return edgeInfos;
+        }
+
+        public void setEdgeInfos(List<EdgeInfo<V, E>> edgeInfos) {
+            this.edgeInfos = edgeInfos;
+        }
+
+        @Override
+        public String toString() {
+            return "PathInfo{" + "weight=" + weight + ", edgeInfos=" + edgeInfos + '}';
+        }
+    }
+
+    public abstract Set<EdgeInfo<V, E>> mst();
+
+    public static class EdgeInfo<V, E>{
+
+        V from;
+        V to;
+        E weight;
+
+        public EdgeInfo(V from, V to, E weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+
+
+
+        public V getFrom() {
+            return from;
+        }
+
+        public void setFrom(V from) {
+            this.from = from;
+        }
+
+        public V getTo() {
+            return to;
+        }
+
+        public void setTo(V to) {
+            this.to = to;
+        }
+
+        @Override
+        public String toString() {
+            return "EdgeInfo{" +
+                    "from=" + from +
+                    ", to=" + to +
+                    ", weight=" + weight +
+                    '}';
+        }
+
+        public E getWeight() {
+            return weight;
+        }
+
+        public void setWeight(E weight) {
+            this.weight = weight;
+        }
+
+
+    }
+
     public interface VertexVisitor<V>{
         boolean visit(V v);
 
+    }
+
+    public interface WeightManager<E>{
+        int compare(E w1, E w2);
+        E add(E w1, E w2);
     }
 
 }
--- src/ca/bytetube/_10_graph/ListGraph.java
+++ src/ca/bytetube/_10_graph/ListGraph.java
@@ -7,7 +7,16 @@ public class ListGraph<V, E> extends Graph<V, E> {
 
     private Set<Edge<V, E>> edges = new HashSet<Edge<V, E>>();
 
-    private static class Vertex<V, E>{
+    private Comparator<Edge<V, E>> edgeComparator = new Comparator<Edge<V, E>>() {
+        @Override
+        public int compare(Edge<V, E> o1, Edge<V, E> o2) {
+            return weightManager.compare(o1.weight, o2.weight);
+        }
+    };
+
+
+
+    private static class Vertex<V, E> {
         V value;
         Set<Edge<V, E>> inEdges = new HashSet<>();
         Set<Edge<V, E>> outEdges = new HashSet<>();
@@ -23,9 +32,10 @@ public class ListGraph<V, E> extends Graph<V, E> {
             Vertex<?, ?> vertex = (Vertex<?, ?>) o;
             return Objects.equals(value, vertex.value) && Objects.equals(inEdges, vertex.inEdges) && Objects.equals(outEdges, vertex.outEdges);
         }
+
         @Override
         public int hashCode() {
-            return value == null ? 0 :value.hashCode();
+            return value == null ? 0 : value.hashCode();
         }
 
         @Override
@@ -34,7 +44,7 @@ public class ListGraph<V, E> extends Graph<V, E> {
         }
     }
 
-    private static class Edge<V, E>{
+    private static class Edge<V, E> {
         E weight;
         Vertex<V, E> from;
         Vertex<V, E> to;
@@ -65,6 +75,10 @@ public class ListGraph<V, E> extends Graph<V, E> {
                     ", to=" + to +
                     "weight=" + weight +
                     '}';
+        }
+
+        public EdgeInfo<V, E> info() {
+            return new EdgeInfo(from, to, weight);
         }
     }
 
@@ -108,7 +122,7 @@ public class ListGraph<V, E> extends Graph<V, E> {
         //不管之前存在与否， 都尝试删除这条边， 在重新建立新的边
         Edge<V, E> edge = new Edge<V, E>(fromVertex, toVertex);
         edge.weight = weight;
-        if (fromVertex.outEdges.remove(edge)){
+        if (fromVertex.outEdges.remove(edge)) {
             toVertex.inEdges.remove(edge);
             edges.remove(edge);
         }
@@ -128,7 +142,7 @@ public class ListGraph<V, E> extends Graph<V, E> {
         if (toVertex == null) return;
 
         Edge<V, E> edge = new Edge<V, E>(fromVertex, toVertex);
-        if (fromVertex.outEdges.remove(edge)){
+        if (fromVertex.outEdges.remove(edge)) {
             toVertex.inEdges.remove(edge);
             edges.remove(edge);
         }
@@ -151,14 +165,14 @@ public class ListGraph<V, E> extends Graph<V, E> {
 //            edges.remove(edge);
 //        } concurrent modification error 删除的是的后需要用iterator删除 避免dead lock 会将其他运行的发生冲突 会产生并发修改异常
 
-        for (Iterator<Edge<V, E>> iterator = vertex.outEdges.iterator(); iterator.hasNext();){
+        for (Iterator<Edge<V, E>> iterator = vertex.outEdges.iterator(); iterator.hasNext(); ) {
             Edge<V, E> edge = iterator.next();
             edge.to.inEdges.remove(edge);
             iterator.remove();
             edges.remove(edge);
         }
 
-        for (Iterator<Edge<V, E>> iterator = vertex.inEdges.iterator(); iterator.hasNext();){
+        for (Iterator<Edge<V, E>> iterator = vertex.inEdges.iterator(); iterator.hasNext(); ) {
             Edge<V, E> edge = iterator.next();
             edge.from.outEdges.remove(edge);
             iterator.remove();
@@ -177,7 +191,7 @@ public class ListGraph<V, E> extends Graph<V, E> {
         queue.add(beginVertex);
         visitedVertices.add(beginVertex);
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
 
             Vertex<V, E> poll = queue.poll();
             System.out.print(poll.value + "");
@@ -203,7 +217,7 @@ public class ListGraph<V, E> extends Graph<V, E> {
         queue.add(beginVertex);
         visitedVertices.add(beginVertex);
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Vertex<V, E> poll = queue.poll();
             if (visitor.visit(poll.value)) return;
             System.out.print(poll.value + "");
@@ -217,7 +231,6 @@ public class ListGraph<V, E> extends Graph<V, E> {
 
         }
     }
-
 
 
 // recursion edition for dfs
@@ -256,10 +269,10 @@ public class ListGraph<V, E> extends Graph<V, E> {
         stack.push(beginVertex);
         System.out.print(beginVertex.value + " ");
         visitedVertices.add(beginVertex);
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
 //            2. 选择弹出顶点的的一条outage， 将这条的from to按顺序放入栈中
             Vertex<V, E> vertex = stack.pop();
-            for (Edge<V, E> edge : vertex.outEdges){
+            for (Edge<V, E> edge : vertex.outEdges) {
                 if (visitedVertices.contains(edge.to)) continue;
                 stack.push(edge.from);
                 stack.push(edge.to);
@@ -283,11 +296,11 @@ public class ListGraph<V, E> extends Graph<V, E> {
         stack.push(beginVertex);
         System.out.print(beginVertex.value + " ");
         visitedVertices.add(beginVertex);
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
 //            2. 选择弹出顶点的的一条outage， 将这条的from to按顺序放入栈中
             Vertex<V, E> vertex = stack.pop();
             if (visitor.visit(vertex.value)) return;
-            for (Edge<V, E> edge : vertex.outEdges){
+            for (Edge<V, E> edge : vertex.outEdges) {
                 if (visitedVertices.contains(edge.to)) continue;
                 stack.push(edge.from);
                 stack.push(edge.to);
@@ -301,13 +314,11 @@ public class ListGraph<V, E> extends Graph<V, E> {
 
 
     /**
-     *
      * 1. 需要准备一个map 用来存储图的inDegree信息， 一个queue（缓存） 一个list（存储结果）
      * 2. 将graph中inDegree=0 的顶点放入queue， inDegree ！= 0的顶点放入map中
      * 3. 出队queue的队头原粗， 放入list中， 并且更新map中的inDegree信息
      * 4. 从map找到inDegree = 0 的顶点， 并放入queue中
      * 5. 不断重复3， 4 过程直到queue为空
-     *
      */
     @Override
     public List<V> toPoLogicalSort(V begin) {
@@ -324,11 +335,11 @@ public class ListGraph<V, E> extends Graph<V, E> {
         });
 
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             //3. 出队queue的队头原粗， 放入list中， 并且更新map中的inDegree信息
             Vertex<V, E> vertex = queue.poll();
             list.add(vertex.value);
-            for (Edge<V, E> edge :vertex.outEdges) {
+            for (Edge<V, E> edge : vertex.outEdges) {
                 int toIn = map.get(edge.to) - 1;
                 //4. 从map找到inDegree = 0 的顶点， 并放入queue中
                 if (toIn == 0) queue.offer(edge.to);
@@ -336,6 +347,211 @@ public class ListGraph<V, E> extends Graph<V, E> {
             }
         }
         return list;
+    }
+
+
+
+    @Override
+    public Set<EdgeInfo<V, E>> mst() {
+        return prim();
+    }
+
+    private Set<EdgeInfo<V, E>> prim() {
+        //A mst 边集
+        Set<EdgeInfo<V, E>> edgeInfo = new HashSet<>();
+
+
+        //S mst 点集
+        Set<Vertex<V, E>> addedVertices = new HashSet<>();
+
+        Iterator<Vertex<V, E>> iterator = vertices.values().iterator();
+        Vertex<V, E> vertex = iterator.next();
+        addedVertices.add(vertex);
+        //minHeap(vertex.outEdges, edgeComparator)
+        //从堆顶去除最小节点
+        //Edge<V, E> edge = minHeap.remove();
+//        if (addedVertices.contains(edge.to)) continue;
+        //AB ---> A mst 边集
+        //edgeInfo.add(edge.info());
+        ////B ---> B mst 点集
+        // addedVertices.add(edge.to);
+
+        //将所有B的out edges放入Heap中， 继续寻找边集中的最小crossing out edges
+        //minHeap.addAll(edge.to.outedge);
+        return edgeInfo;
+    }
+
+    private Set<EdgeInfo<V, E>> kruscal() {
+        int edgeSize = edges.size() - 1;
+        if (edgeSize == -1) return null;
+        Set<EdgeInfo<V, E>> edgeInfo = new HashSet<>();
+        // minHeap edges, edgeComparator
+
+        //hub judge whether it will be a circle if added
+        //UnionFind
+
+        return edgeInfo;
+
+    }
+
+    //shortest path algorithm
+    @Override
+    public Map<V, E> shortestPathWithoutPathInfos(V begin) {
+        Vertex<V, E> beginVertex = vertices.get(begin);
+        if (beginVertex == null) return null;
+
+        Map<Vertex<V, E>, E> paths = new HashMap<>(); //红色容器
+        Map<V, E> selectedPaths = new HashMap<>(); //绿色容器
+
+        /**
+         * Map<V, E> paths = new HashMap<>();
+         * map.put("B", 10);
+         * map.put("D", 30);
+         * map.put("E", 100);
+         *
+         * 从 paths中找到最小权重， 即是第一个起飞点： 找到B点
+         * 如果继续把B留在map中， 可能会被重复选择， 所以可能会选择删除起始点B
+         * 因此我们需要再增加一个map用来存储已经走过的路径
+         * */
+
+        //1. 初始化paths：将B, D, E的当前的最短路径先装入map
+        for (Edge<V, E> edge :beginVertex.outEdges){
+            paths.put(edge.to, edge.weight);
+        }
+
+        while (!paths.isEmpty()){
+            Map.Entry<Vertex<V, E>, E> minEntry = getMinPathWithoutPathInfos(paths); //("B", 10)
+
+            Vertex<V, E> minVertex = minEntry.getKey();
+
+            E minWeight = minEntry.getValue();
+            selectedPaths.put(minVertex.value, minWeight); //B点起飞
+            paths.remove(minVertex);
+
+
+            //Relaxation
+            //对起飞点所有的outEdges分别计算更新后的weight和老的weight， 然后进行大小比较
+            //如果newWeight小于oldWeight，则更新paths
+            //否则， 不用更新
+
+            //对起飞点所有的outEdges分别计算更新后的weight
+            for (Edge<V, E> edge : minVertex.outEdges){
+                //计算newWeight
+                E newWeight = weightManager.add(minEntry.getValue(), edge.weight);
+                //计算oldWeight
+                E oldWeight = paths.get(edge.to);
+                if (oldWeight == null || weightManager.compare(newWeight, oldWeight) < 0){
+                    paths.put(edge.to, newWeight);
+                }
+
+            }
+        }
+
+
+
+        return selectedPaths;
+    }
+
+    //返回值是map中的一个entry 即ending point 和 weight
+    private Map.Entry<Vertex<V, E>, E> getMinPathWithoutPathInfos(Map<Vertex<V,E>,E> paths) {
+        Iterator<Map.Entry<Vertex<V, E>, E>> iterator = paths.entrySet().iterator();
+        Map.Entry<Vertex<V, E>, E> minEntry = iterator.next();
+        while (iterator.hasNext()){
+            Map.Entry<Vertex<V, E>, E> nextEntry = iterator.next();
+            if (weightManager.compare(nextEntry.getValue(), minEntry.getValue()) < 0){
+                minEntry = nextEntry;
+            }
+        }
+        return minEntry;
+    }
+
+
+    @Override
+    public Map<V, PathInfo<V, E>> shortestPath(V begin) {
+        return dijkstra(begin);
+    }
+
+    public Map<V, PathInfo<V, E>> dijkstra(V begin) {
+
+        Vertex<V, E> beginVertex = vertices.get(begin);
+        if (beginVertex == null) return null;
+
+        Map<Vertex<V, E>, PathInfo<V, E>> paths = new HashMap<>(); //红色容器
+        Map<V, PathInfo<V, E>> selectedPaths = new HashMap<>(); //绿色容器
+
+        /**
+         * Map<V, E> paths = new HashMap<>();
+         * map.put("B", 10);
+         * map.put("D", 30);
+         * map.put("E", 100);
+         *
+         * 从 paths中找到最小权重， 即是第一个起飞点： 找到B点
+         * 如果继续把B留在map中， 可能会被重复选择， 所以可能会选择删除起始点B
+         * 因此我们需要再增加一个map用来存储已经走过的路径
+         * */
+
+        //1. 初始化paths：将B, D, E的当前的最短路径先装入map
+        for (Edge<V, E> edge :beginVertex.outEdges){
+            PathInfo<V, E> path = new PathInfo<>();
+            path.weight = edge.weight;
+            path.edgeInfos.add(edge.info());
+            paths.put(edge.to, path);
+        }
+
+        while (!paths.isEmpty()){
+            Map.Entry<Vertex<V, E>, PathInfo<V, E>> minEntry = getMinPath(paths); //("B", 10)
+
+            Vertex<V, E> minVertex = minEntry.getKey();
+            PathInfo<V, E> minPath = minEntry.getValue();
+
+            selectedPaths.put(minVertex.value, minPath); //B点起飞
+            paths.remove(minVertex);
+
+
+            //Relaxation
+            //对起飞点所有的outEdges分别计算更新后的weight和老的weight， 然后进行大小比较
+            //如果newWeight小于oldWeight，则更新paths
+            //否则， 不用更新
+
+            //对起飞点所有的outEdges分别计算更新后的weight
+            for (Edge<V, E> edge : minVertex.outEdges){
+//                计算newWeight
+//                E newWeight = weightManager.add(minEntry.getValue(), edge.weight);
+//                //计算oldWeight
+//                E oldWeight = paths.get(edge.to);
+//                if (oldWeight == null || weightManager.compare(newWeight, oldWeight) < 0){
+//                    paths.put(edge.to, newWeight);
+//                }
+                if (selectedPaths.containsKey(edge.to.value)) continue;
+                dijkstraRelaxation(edge, paths, minPath);
+            }
+        }
+        return selectedPaths;
+    }
+
+    private void dijkstraRelaxation(Edge<V, E> edge, Map<Vertex<V, E>, PathInfo<V, E>> paths, PathInfo<V, E> minPath){
+        //                计算newWeight
+                E newWeight = weightManager.add(minPath.weight, edge.weight);
+                PathInfo<V, E> oldPath = paths.get(edge.to);
+                if (oldPath == null || weightManager.compare(newWeight, oldPath.weight) < 0){
+                    PathInfo<V,E> path = new PathInfo<>();
+                    path.weight = newWeight;
+                    path.edgeInfos.addAll(minPath.edgeInfos);
+                    path.edgeInfos.add(edge.info());
+                    paths.put(edge.to, path);
+                }
+
+    }
+    private Map.Entry<Vertex<V, E>, PathInfo<V, E>> getMinPath(Map<Vertex<V, E>, PathInfo<V, E>> paths) {
+        Iterator<Map.Entry<Vertex<V, E>, PathInfo<V, E>>> iterator = paths.entrySet().iterator();
+        Map.Entry<Vertex<V, E>, PathInfo<V, E>> minEntry = iterator.next();
+        while (iterator.hasNext()){
+            Map.Entry<Vertex<V, E>, PathInfo<V, E>> nextEntry = iterator.next();
+            if (weightManager.compare(nextEntry.getValue().weight, minEntry.getValue().weight) < 0){
+                minEntry = nextEntry;
+            }
+        }
+        return minEntry;
     }
 
 }
